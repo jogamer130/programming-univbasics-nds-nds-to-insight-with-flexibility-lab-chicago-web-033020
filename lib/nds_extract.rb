56d1a99eb11b3a4a850b32c5a2cbb7896d9e433a
Derek Williams <jogamer130@gmail.com>
2020-03-20 22:07:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,6 +52,17 @@ def movies_with_director_key(name, movies_collection)
   # movie_with_director_name method
   
   
+  result = []
+  index = 0
+
+  while index < movies_collection.length do
+    movie_data = movies_collection[index]
+    result << movie_with_director_name(name, movie_data)
+    index += 1
+  end
+
+  result
+  
 end
 
 
